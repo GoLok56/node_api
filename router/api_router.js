8ca24ff8a3a4a191a60fe2264cad3eb43c34447a
GoLok56 <satadii11@gmail.com>
2017-08-07 21:07:35 +0700

    add basic skeleton

--- router/api_router.js
+++ router/api_router.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
+// Get token for the user
+router.post('/authenticate', function(req, res){
+    res.send('POST');
+});
+
+// Retrieving all available user
+router.get('/user', function(req, res){
     res.send("GAY GAY GAY GAY GAY GAY");
+});
+
+// Updating the user info
+router.put('/user/:id', function(req, res){
+
+});
+
+// Delete the user
+router.delete('/user/:id', function(req, res){
+
 });
 
 module.exports = router;
