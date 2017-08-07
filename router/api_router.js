4585124cc3c024ee4871bc1ecf93ce957ba519b7
GoLok56 <satadii11@gmail.com>
2017-08-07 23:17:19 +0700

    add register feature

--- router/api_router.js
+++ router/api_router.js
@@ -1,5 +1,21 @@
 var express = require('express');
+var User = require('../model/User');
+
 var router = express.Router();
+
+// Register a new user to database
+router.post('/register', function(req, res){
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    }, function(err, user){
+        if(err) throw err;
+        console.log("New user added!");
+        res.json({data: user});
+    });
+});
 
 // Get token for the user
 router.post('/authenticate', function(req, res){
@@ -8,17 +24,17 @@ router.post('/authenticate', function(req, res){
 
 // Retrieving all available user
 router.get('/user', function(req, res){
-    res.send("GAY GAY GAY GAY GAY GAY");
+    res.send("GET");
 });
 
 // Updating the user info
 router.put('/user/:id', function(req, res){
-
+    res.send('PUT');
 });
 
 // Delete the user
 router.delete('/user/:id', function(req, res){
-
+    res.send('DELETE');
 });
 
 module.exports = router;
