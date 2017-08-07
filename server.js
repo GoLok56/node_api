0efd5a39df04c1572fe57a3775896ee2e6657720
GoLok56 <satadii11@gmail.com>
2017-08-07 22:48:00 +0700

    open connection to database

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+// Setting up the package
 var express = require('express');
+var mongoose = require('mongoose');
 var config = require('./config');
 
 var app = express();
 
-app.use('/', require('./router/api_router'));
+// Connecting to database
+mongoose.createConnection(config.DATABASE, {useMongoClient: true});
 
+// Setting up the middleware
+app.use('/api', require('./router/api_router'));
+
+// Starting the server
 app.listen(config.PORT);
 console.log('Server is running...');
